6a56f453f0642dc364776dca2024aa0d4c0456ba
AbdelrhmanAhmedFathy <121230612+AbdelrhmanAhmedFathy@users.noreply.github.com>
2022-12-22 16:44:41 +0200

    Add files via upload

---
+++ Prefect numbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, sum, mn, mx;
+
+            Console.WriteLine("ENTER THE START RANGE");
+            mn = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("ENTER THE END RANGE");
+            mx = int.Parse(Console.ReadLine());
+
+            for (n=mn; n<=mx; n++)
+            {
+                i = 1;
+                sum =0;
+                while (i<n)
+                {
+                    if (n % i == 0)
+                        sum += i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
