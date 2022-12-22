82f0c0ac178ebdd68c376485f0dcaa837402d826
AbdelrhmanAhmedFathy <121230612+AbdelrhmanAhmedFathy@users.noreply.github.com>
2022-12-22 15:55:41 +0200

    Add files via upload

---
+++ Prime numbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, x;
+
+            Console.WriteLine("enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+
+            num = n1;
+            while (num<=n2)
+            {
+                x = 0;
+                for (i=2; i<num; i++)
+                {
+                    if (num%i==0)
+                    {
+                        x = 1;
+                        break;
+                    }
+                }
+                if (x == 0)
+                    Console.WriteLine(num);
+                num++;
+            }
+            return;
+            Console.ReadKey();
+        }
+    }
+}
